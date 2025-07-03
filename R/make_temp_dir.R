14b0d14b5ddce20cd66c4c6cf58dd6b4fafe218a
Johannes Feldbauer <johannes.feldbauer@tu-dresden.de>
2025-07-03 14:02:27 +0200

    fixed issue with tmp_dir

--- R/make_temp_dir.R
+++ R/make_temp_dir.R
@@ -23,6 +23,8 @@ make_temp_dir <- function(model, folder = ".", n = 2, tmp_dir = NULL) {
   temp_dirs <- sapply(1:n, \(n) {
     if(is.null(tmp_dir)) {
       tmp_dir <- tempdir()
+    } else {
+      tmp_dir <- file.path(oldwd, tmp_dir)
     }
     dir <- file.path(tmp_dir, paste0("n_", n), model)
     unlink(dir, recursive = TRUE, force = TRUE)
